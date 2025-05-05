5c978857486a2b8483f5fdc091f9ed273fa5f680
Andriy <andrii.kovalchuk314@gmail.com>
2025-05-05 13:53:15 +0200

    Premier commit

---
+++ GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/platform/bootloader/app_properties/app_properties.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.4.6/platform/bootloader/app_properties/app_properties.o: \
+ C:/Users/andri/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/app_properties/app_properties.c \
+ c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\config/app_properties_config.h \
+ C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\autogen/sl_application_type.h \
+ c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h
+c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\config/app_properties_config.h:
+C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\autogen/sl_application_type.h:
+c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h:
---
+++ GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/platform/service/system/src/sl_system_process_action.d
@@ -0,0 +1,6 @@
+gecko_sdk_4.4.6/platform/service/system/src/sl_system_process_action.o: \
+ C:/Users/andri/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_process_action.c \
+ c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\autogen/sl_event_handler.h
+c:\users\andri\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C:\Users\andri\SimplicityStudio\v5_workspace\soc_empty_AK_KM\autogen/sl_event_handler.h:
